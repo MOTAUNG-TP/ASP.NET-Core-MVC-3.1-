3dfda6ccf3a58c1c5ee37c3c9d13bf1e778d4fe1
Thato Percy Motaung <motaungthatopercy@outlook.com>
2022-05-16 15:22:29 +0200

    Added MVC to the Application in the Startup.cs file and changed the default route to call the basic action method in the basi e.g. controller

---
+++ MOTAUNG_TP.BookStore/MOTAUNG_TP.BookStore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MOTAUNG_TP.BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "MOTAUNG_TP";
+        }
+    }
+}
